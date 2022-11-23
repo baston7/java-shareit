caa05fbbfd7f5f989495bf8403dfdf3f13508bc2
Sergey <sleontiev777@gmail.com>
2022-11-23 23:29:57 +0300

    15 спринт. add-item-requests 23.11.22  tests finish

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -13,40 +13,49 @@ import java.util.Optional;
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     //поиск аренд создателя в будущем
     List<Booking> findByBooker_IdAndStartIsAfterAndEndIsAfterOrderByEndDesc(long bookerId, LocalDateTime time1,
-                                                                            LocalDateTime time2, PageRequest pageRequest);
+                                                                            LocalDateTime time2,
+                                                                            PageRequest pageRequest);
 
     //поиск всех аренд создателя
     List<Booking> findByBooker_IdOrderByEndDesc(long bookerID, PageRequest pageRequest);
 
     //поиск текущих аренд создателя
     List<Booking> findByBooker_IdAndStartIsBeforeAndEndIsAfterOrderByEndDesc(long bookerId, LocalDateTime time1,
-                                                                             LocalDateTime time2, PageRequest pageRequest);
+                                                                             LocalDateTime time2,
+                                                                             PageRequest pageRequest);
 
     //поиск прошедших аренд создателя
     List<Booking> findByBooker_IdAndStartIsBeforeAndEndIsBeforeOrderByEndDesc(long bookerId, LocalDateTime time1,
-                                                                              LocalDateTime time2,PageRequest pageRequest);
+                                                                              LocalDateTime time2,
+                                                                              PageRequest pageRequest);
+
     List<Booking> findByBooker_IdAndStartIsBeforeAndEndIsBeforeOrderByEndDesc(long bookerId, LocalDateTime time1,
                                                                               LocalDateTime time2);
+
     //поиск аренд создателя по статусу заявки
-    List<Booking> findByBooker_IdAndStatusEqualsOrderByEndDesc(long bookerID, Status status,PageRequest pageRequest);
+    List<Booking> findByBooker_IdAndStatusEqualsOrderByEndDesc(long bookerID, Status status, PageRequest pageRequest);
 
     //поиск будущих аренд по владельцу вещи
     List<Booking> findByItem_Owner_IdAndStartIsAfterAndEndIsAfterOrderByEndDesc(long ownerId, LocalDateTime time1,
-                                                                                LocalDateTime time2,PageRequest pageRequest);
+                                                                                LocalDateTime time2,
+                                                                                PageRequest pageRequest);
 
     //поиск всех аренд по владельцу вещи
-    List<Booking> findByItem_Owner_IdOrderByEndDesc(long ownerId,PageRequest pageRequest);
+    List<Booking> findByItem_Owner_IdOrderByEndDesc(long ownerId, PageRequest pageRequest);
 
     //поиск текущих аренд по владельцу вещи
     List<Booking> findByItem_Owner_IdAndStartIsBeforeAndEndIsAfterOrderByEndDesc(long ownerId, LocalDateTime time1,
-                                                                                 LocalDateTime time2,PageRequest pageRequest);
+                                                                                 LocalDateTime time2,
+                                                                                 PageRequest pageRequest);
 
     //поиск прошлых аренд по владельцу вещи
     List<Booking> findByItem_Owner_IdAndStartIsBeforeAndEndIsBeforeOrderByEndDesc(long ownerId, LocalDateTime time1,
-                                                                                  LocalDateTime time2,PageRequest pageRequest);
+                                                                                  LocalDateTime time2,
+                                                                                  PageRequest pageRequest);
 
     //поиск аренд по владельцу вещи и статусу заявки
-    List<Booking> findByItem_Owner_IdAndStatusEqualsOrderByEndDesc(long ownerId, Status status,PageRequest pageRequest);
+    List<Booking> findByItem_Owner_IdAndStatusEqualsOrderByEndDesc(long ownerId, Status status,
+                                                                   PageRequest pageRequest);
 
     //поиск первой будущей аренды по вещи, где статусы заявки не соответсвуют заданным
     Optional<Booking> findTopByItem_IdAndStartIsAfterAndStatusIsNotAndStatusIsNotOrderByEndDesc(long itemId,
