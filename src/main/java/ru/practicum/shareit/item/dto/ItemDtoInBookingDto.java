83c6c47b6ccefd2053475044fd910ff6777060f5
Sergey <sleontiev777@gmail.com>
2022-11-24 23:44:00 +0300

    15 спринт. add-item-requests 24.11.22  version 2

--- src/main/java/ru/practicum/shareit/item/dto/ItemDtoInBookingDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDtoInBookingDto.java
@@ -1,8 +1,10 @@
 package ru.practicum.shareit.item.dto;
 
 import lombok.*;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+
 @Getter
 @AllArgsConstructor
 public class ItemDtoInBookingDto {
--- src/main/java/ru/practicum/shareit/item/dto/ItemDtoToUser.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDtoToUser.java
@@ -3,12 +3,10 @@ package ru.practicum.shareit.item.dto;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import ru.practicum.shareit.booking.dto.BookingDtoToOwnerItem;
 
 import java.util.List;
-
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
