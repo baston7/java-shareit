ea3803b163fd74cd7d7d9917cb1c5d962b00597d
Sergey <sleontiev777@gmail.com>
2022-12-04 21:29:01 +0300

    16 спринт.

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,3 +1,4 @@
 cp -rf ./tests/checkstyle.xml ./checkstyle.xml
 cp -rf ./tests/suppressions.xml ./suppressions.xml
-mvn verify -P check --no-transfer-progress
+mvn verify -P check,coverage --no-transfer-progress
+docker-compose build
